a49fbd0ef133819947f7d37e7ca96d626069a3ed
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-06 01:52:33 +0300

    made task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const ADMIN_PASSWORD = "jqueryismyjam";
+let userPassword = prompt("Введите пароль");
+
+if (userPassword == null) {
+  let message = "Отменено пользователем!";
+  alert(message);
+} else {
+  if (userPassword == ADMIN_PASSWORD) {
+    let message = "Добро пожаловать!";
+    alert(message);
+  }
+  if (userPassword != ADMIN_PASSWORD) {
+    let message = "Доступ запрещен, неверный пароль!";
+    alert(message);
+  }
+}
