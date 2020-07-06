6c206b8dddd17eece0e512b16ca5d27a4ddaa676
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-06 13:59:36 +0300

    made task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const credits = 23580;
+const pricePerDroid = 3000;
+
+let userOrder = prompt("Введите количество дроидов к заказу", " ");
+
+if (userOrder == null) {
+  let message = "Отменено пользователем!";
+
+  console.log(message);
+} else {
+  let totalPrice = userOrder * pricePerDroid;
+  console.log(`Общая цена заказа ${totalPrice} кредитов`);
+
+  if (totalPrice > credits) {
+    console.log("Недостаточно средств на счету!");
+  }
+  if (totalPrice < credits) {
+    let userCreditsBalance = credits - totalPrice;
+    console.log (`Вы купили ${userOrder} дроидов, на счету осталось ${userCreditsBalance} кредитов.`)
+  }
+}
