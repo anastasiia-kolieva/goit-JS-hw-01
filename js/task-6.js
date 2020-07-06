2e2346cb6ee3d114fc3bef8630062968364bc1c7
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-06 19:12:49 +0300

    Tried to correct cycle do...while

--- js/task-6.js
+++ js/task-6.js
@@ -4,15 +4,14 @@ let total = 0;
 const x = NaN;
 do {
   let input = prompt("Введите число.");
-  if (input !== null) {
-    if (input != x) {
-      total += input;
-      continue;
-    } else {
-      alert("Было введено не число, попробуйте еще раз");
-      total = total;
-      continue;
-    }
+
+  if (input != x) {
+    total += input;
+    continue;
+  } else {
+    alert("Было введено не число, попробуйте еще раз");
+    total = total;
+    continue;
   }
 } while (input == null);
 
