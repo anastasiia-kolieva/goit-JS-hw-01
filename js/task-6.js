07192c26b448c3c1bdc4797fc730208bc72dadb4
anastasiia-kolieva <nastya1110@gmail.com>
2020-07-06 19:03:54 +0300

    Tried to make task 6. Had problems

--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,19 @@
 "use strict";
+let input;
+let total = 0;
+const x = NaN;
+do {
+  let input = prompt("Введите число.");
+  if (input !== null) {
+    if (input != x) {
+      total += input;
+      continue;
+    } else {
+      alert("Было введено не число, попробуйте еще раз");
+      total = total;
+      continue;
+    }
+  }
+} while (input == null);
+
+alert(`Общая сумма чисел равна ${total}`);
